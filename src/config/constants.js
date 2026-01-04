48508198d4bc1d366f5f7b4c5854afbc50da4c2f
Wei Cheng <goedmonton@gmail.com>
2026-01-03 21:11:50 -0700

    Rename RegistrationPage(2)

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,3 +2,4 @@ export const AUTH_API_END_POINT = "http://localhost:5000/api/auth";
 export const USER_API_END_POINT = "http://localhost:5000/api/user";
 export const ARTICLE_API_END_POINT = "http://localhost:5000/api/article";
 export const VIDEO_API_END_POINT = "http://localhost:5000/api/video";
+export const CHANNEL_API_END_POINT = "http://localhost:5000/api/channel";
